cf00beb50934eb74da70733b663590040388c0a0
zhiwutian <brettalbright@outlook.com>
2018-04-24 15:13:49 -0700

    ES6 practice and learning super

---
+++ app/helpers.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+
+export function addToDom(text) {
+    $("#root").append(text);
+
+}
+
--- app/index.js
+++ app/index.js
@@ -1,1 +1,14 @@
-console.log('App working: open app/index.js to start exploring ES6');
+import {addToDom} from "./helpers"; // named import syntax
+import add from "./add"; // default import syntax
+import SuperPerson from "./person";
+
+addToDom("<hr>");
+
+const John = new SuperPerson("John", "Find it Man!");
+const Kara = new SuperPerson("Kara", "It wasn't me Woman");
+addToDom(John.speak("Hello"));
+addToDom(Kara.speak("How are you?"));
+
+addToDom(Kara.xRay());
+
+addToDom(John.sayHeroName());
